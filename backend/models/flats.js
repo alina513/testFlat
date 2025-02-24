67ae96085179c98c8c45ea7add5f0dc3302d48d8
alina513 <130853290+alina513@users.noreply.github.com>
2025-02-24 19:01:03 +0200

    Update flats.js

--- backend/models/flats.js
+++ backend/models/flats.js
@@ -2,9 +2,9 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
 const formSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
+  name: { type: String },
+  email: { type: String },
+  phone: { type: String },
   message: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
@@ -56,10 +56,10 @@ const updateFlateSchema = Joi.object({
 });
 
 const contactFormSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().required(),
-  message: Joi.string().optional(),
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+  message: Joi.string(),
 });
 
 const schemas = { createFlatSchema, updateFlateSchema, contactFormSchema };
